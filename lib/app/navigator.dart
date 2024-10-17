7ef3fc4ef56f2d251c749e5aaeed43c13627abd0
Cássio Meira <cassiomeira_12@hotmail.com>
2024-10-17 19:52:24 -0300

    fix: update app name

--- lib/app/navigator.dart
+++ lib/app/navigator.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:foxbit_hiring_test_template/app/pages/home/home_view.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
-  '/': (BuildContext context) => HomePage(),
+  '/': (BuildContext context) => const HomePage(),
 };
